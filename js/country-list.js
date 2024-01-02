d57939a89339bdfcd85aae967fabf179da9fbb9b
MarkTheBoy <146758649+MarkTheBoy@users.noreply.github.com>
2024-01-02 16:18:10 +0200

    Currency exchange

---
+++ js/country-list.js
@@ -0,0 +1,12 @@
+let country_code = {
+    "USD": "US",
+    "JPY": "JP",
+    "GBP": "GB",
+    "INR": "IN",
+    "MVR": "MV",
+    "EUR" : "DE",
+    "MWK": "MW",
+    "CAD": "CA",
+    "AUD": "AU",
+    "UAH" : "UA",
+}
